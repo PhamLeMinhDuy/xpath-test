64fd375afd8655a42c367365b147aa104e5b6857
MinhDuy17072002 <duy.207ct68618@vanlanguni.vn>
2023-06-10 19:57:42 +0700

    xpath_test1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,66 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        /*ChromeDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+        Thread.sleep(10000);
+        WebElement button = driver.findElement(By.xpath("//a[@class='gb_oa gb_od gb_Pd gb_ne']"));
+        System.out.println(button.getText());
+        button.click();*/
+
+        //Demo blaze
+        ChromeDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.demoblaze.com/#");
+        Thread.sleep(5000);
+        WebElement home = driver.findElement(By.xpath("//li/a[text()='Home ']"));
+        WebElement contact = driver.findElement(By.xpath("//li/a[text()='Contact']"));
+        WebElement about = driver.findElement(By.xpath("//li/a[text()='About us']"));
+        WebElement cart = driver.findElement(By.xpath("//li/a[text()='Cart']"));
+        WebElement login = driver.findElement(By.xpath("//li/a[text()='Log in']"));
+        WebElement signup = driver.findElement(By.xpath("//li/a[text()='Sign up']"));
+        WebElement prev = driver.findElement(By.xpath("//a[@class='carousel-control-prev']"));
+        WebElement next = driver.findElement(By.xpath("//a[@class='carousel-control-next']"));
+        WebElement categories = driver.findElement(By.xpath("//a[text()='CATEGORIES']"));
+        WebElement phones = driver.findElement(By.xpath("//a[text()='Phones']"));
+        WebElement laptops = driver.findElement(By.xpath("//a[text()='Laptops']"));
+        WebElement lumia1520 = driver.findElement(By.xpath("//div[@class='card h-100']//a"));
+        WebElement monitors = driver.findElement(By.xpath("//a[text()='Monitors']"));
+        /*System.out.println(home.getText());
+        home.click();
+        System.out.println(contact.getText());
+        contact.click();
+        System.out.println(about.getText());
+        about.click();
+        System.out.println(cart.getText());
+        cart.click();
+        System.out.println(login.getText());
+        login.click();
+        System.out.println(signup.getText());
+        signup.click();
+        System.out.println(prev.getText());
+        prev.click();
+        System.out.println(next.getText());
+        next.click();*/
+        /*System.out.println(categories.getText());
+        categories.click();
+        System.out.println(phones.getText());
+        phones.click();
+        System.out.println(laptops.getText());
+        laptops.click();
+        System.out.println(monitors.getText());
+        monitors.click();*/
+        System.out.println(lumia1520.getText());
+        lumia1520.click();
+       /* System.out.println(laptops.getText());
+        laptops.click();*/
+
+    }
+}
